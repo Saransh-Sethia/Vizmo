a8caea65a00bc736badd272a865a479bd19ae304
saransh <sethia.saransh.ss@gmail.com>
2024-06-24 19:23:22 +0530

    my second commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,21 @@
+const authService = require("../services/authService");
+
+
+const register = async (req, res) => {
+    try {
+      const userData = req.body;
+  
+      const user = await authService.register(userData);
+  
+      // console.log("user", user);
+  
+      res.status(201).json({
+        message: "User Registered Successfully",
+        userId: user._id,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+  module.exports = {register}
---
+++ src/services/authService.js
@@ -0,0 +1,25 @@
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const register = async(userData) => {
+    try{
+const existingUser = await User.findOne({email: userData.email});
+
+if(existingUser){
+    throw new Error('User Already Exists');
+}
+
+const user = new User(userData);
+const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(userData.password, salt);
+user.password = hashedPassword;
+
+await user.save();
+return user;
+    } catch(error){
+        throw error;
+    }
+};
+
+module.exports = {register}
